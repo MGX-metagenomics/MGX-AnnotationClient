3b7f4b654c63fa3a3953a90aab22b5eea8905836
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2023-05-05 15:20:31 +0200

    add maven check

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+command -v mvn >/dev/null 2>&1 || { echo >&2 "mvn not found."; exit 1; }
+
 git clone -b utilities_2_0 https://github.com/MGX-metagenomics/utilities.git
 cd utilities
 mvn install -DskipTests=true
